bbca35ab5a6a40fe7694978710d11160743a1278
Srihari S <ae21b058@smail.iitm.ac.in>
2023-04-07 14:46:04 +0530

    question 1 complete

--- question1.py
+++ question1.py
@@ -1,55 +1,52 @@
 from matplotlib import pyplot as plt
-from scipy.integrate import odeint
 import numpy as np
+def charac_roots(m,k,c):
+    #m = mass, k = spring constant, c = damping coefficient
+    #returns the roots of the characteristic equation
+    roots = np.zeros(2,dtype=complex)
+    roots[1] = (-c + np.emath.sqrt(c**2 - 4*m*k))/(2*m)
+    roots[0] = (-c - np.emath.sqrt(c**2 - 4*m*k))/(2*m)
+    return roots
 
-def diff_eqn(x, t,k,c,m):#defining the differential equation for the first question : 4x''+2500x=0
-    return (x[1],-k*x[0]/m-c*x[1]/m)
-
-
-x0 = [0.15,-20]
-ts=np.linspace(0, 2, 1000)
-us = odeint( diff_eqn, x0, ts,args=(2500,0,4))
-ys=us[:,0]
-amplitude=ys.max()
-plt.figure("1.1")
-plt.plot(ts, ys,'-')
-#plt.plot(ts,ys,'ro')
-plt.xlabel('t')
-plt.ylabel('x')
-plt.show()
-print("The amplitude of the vibration is",amplitude)
-#for question 1.2
-us = odeint( diff_eqn, x0, ts,args=(2500,100,4))
-ys=us[:,0]
-amplitude=ys.max()
-plt.figure("1.2")
-plt.plot(ts, ys,'-')
-#plt.plot(ts,ys,'ro')
-plt.xlabel('t')
-plt.ylabel('x')
-plt.show()
-print("The amplitude of the vibration is",amplitude)
-#for question 1.3
-us = odeint( diff_eqn, x0, ts,args=(2500,200,4))
-ys=us[:,0]
-amplitude=ys.max()
-plt.figure("1.3")
-plt.plot(ts, ys,'-')
-#plt.plot(ts,ys,'ro')
-plt.xlabel('t')
-plt.ylabel('x')
-plt.show()
-print("The amplitude of the vibration is",amplitude)
-#for question 1.4
-us = odeint( diff_eqn, x0, ts,args=(2500,400,4))
-ys=us[:,0]
-amplitude=ys.max()
-plt.figure("1.4")
-plt.plot(ts, ys,'-')
-#plt.plot(ts,ys,'ro')
-plt.xlabel('t')
-plt.ylabel('x')
-plt.show()
-print("The amplitude of the vibration is",amplitude)
+def sol_eqn(x0,v0,t,m,k,c):
+    #x0 = initial position, v0 = initial velocity, t = time, m = mass, k = spring constant, c = damping coefficient
+    #returns the position and velocity of the mass at time t
+    roots = charac_roots(m,k,c)
+    if roots[0].imag != 0 and roots[1].imag != 0:#complex roots case underdamped
+        A = np.array([[1,1],[roots[0],roots[1]]],dtype=complex)
+        B = np.array([x0,v0])
+        const=np.linalg.inv(A).dot(B) #finding the constants of the solution
+        return np.exp(roots[0].real*t)*(const[0]*np.cos(roots[0].imag*t)+const[1]*np.sin(roots[0].imag*t))#x(t)=e^(lambda1t)(C1*cos(omega1t)+C2*sin(omega1t))
+    elif roots[0].imag == 0 and roots[1].imag == 0: 
+        if roots[0].real == roots[1].real:#single root case Critically damped
+            A=np.array([[1,0],[roots[0],roots[0]+1]])
+            B=np.array([x0,v0])
+            const=np.linalg.inv(A).dot(B) #finding the constants of the solution
+            return const[0]*np.exp(roots[0].real*t)+const[1]*t*np.exp(roots[0].real*t)#x(t)=e^(lambda1t)(C1+C2t)
+        else:# overdamped case
+            A=np.array([[1,1],[roots[0],roots[1]]])
+            B=np.array([x0,v0])
+            const=np.linalg.inv(A).dot(B)
+            return const[0]*np.exp(roots[0]*t)+const[1]*np.exp(roots[1]*t)#x(t)=C1*e^(lambda1t)+C2*e^(lambda2t)
 
 
+def plot_sol(x0,v0,t,m,k,c,fignum): #plotting the solution, we only need the real part of the roots
+    plt.figure(fignum)
+    plt.clf()
+    plt.plot(t,sol_eqn(x0,v0,t,m,k,c))
+    plt.title('Damping coefficient = '+str(c))
+    plt.xlabel('Time')
+    plt.ylabel('Position')
+    plt.grid()
+    plt.savefig('question '+fignum+'.png')
+    plt.show()
+
+x0=0.15
+v0=-20
+t=np.linspace(0,2,1000)
+m=4
+k=2500
+c=[0,100,200,400]
+nfig=['1.1','1.2','1.3','1.4']
+for i in range(len(c)):
+    plot_sol(x0,v0,t,m,k,c[i],nfig[i])
