6645488a93edf574e9088f5c695138105e331961
Srihari S <ae21b058@smail.iitm.ac.in>
2023-04-07 10:30:26 +0530

    edits:question1 programm

--- question1.py
+++ question1.py
@@ -2,22 +2,13 @@ from matplotlib import pyplot as plt
 from scipy.integrate import odeint
 import numpy as np
 
-def q1(x, t):#defining the differential equation for the first question : 4x''+2500x=0
-    return (x[1],-2500*x[0]/4-0*x[1]/4)
-
-def q2(x, t):#defining the differential equation for the second question : 4x''+100x'+2500x=0
-    return (x[1],-2500*x[0]/4-100*x[1]/4)
-
-def q3(x, t):#defining the differential equation for the third question : 4x''+200x'+2500x=0
-    return (x[1],-2500*x[0]/4-200*x[1]/4)
-
-def q4(x, t):#defining the differential equation for the first question : 4x''+400x'+2500x=0
-    return (x[1],-2500*x[0]/4-400*x[1]/4)
+def diff_eqn(x, t,k,c,m):#defining the differential equation for the first question : 4x''+2500x=0
+    return (x[1],-k*x[0]/m-c*x[1]/m)
 
 
-x0 = [0.15, 20]
-ts=np.linspace(0, 5, 1000)
-us = odeint( q1, x0, ts)
+x0 = [0.15,-20]
+ts=np.linspace(0, 2, 1000)
+us = odeint( diff_eqn, x0, ts,args=(2500,0,4))
 ys=us[:,0]
 amplitude=ys.max()
 plt.figure("1.1")
@@ -27,4 +18,38 @@ plt.xlabel('t')
 plt.ylabel('x')
 plt.show()
 print("The amplitude of the vibration is",amplitude)
+#for question 1.2
+us = odeint( diff_eqn, x0, ts,args=(2500,100,4))
+ys=us[:,0]
+amplitude=ys.max()
+plt.figure("1.2")
+plt.plot(ts, ys,'-')
+#plt.plot(ts,ys,'ro')
+plt.xlabel('t')
+plt.ylabel('x')
+plt.show()
+print("The amplitude of the vibration is",amplitude)
+#for question 1.3
+us = odeint( diff_eqn, x0, ts,args=(2500,200,4))
+ys=us[:,0]
+amplitude=ys.max()
+plt.figure("1.3")
+plt.plot(ts, ys,'-')
+#plt.plot(ts,ys,'ro')
+plt.xlabel('t')
+plt.ylabel('x')
+plt.show()
+print("The amplitude of the vibration is",amplitude)
+#for question 1.4
+us = odeint( diff_eqn, x0, ts,args=(2500,400,4))
+ys=us[:,0]
+amplitude=ys.max()
+plt.figure("1.4")
+plt.plot(ts, ys,'-')
+#plt.plot(ts,ys,'ro')
+plt.xlabel('t')
+plt.ylabel('x')
+plt.show()
+print("The amplitude of the vibration is",amplitude)
+
 
